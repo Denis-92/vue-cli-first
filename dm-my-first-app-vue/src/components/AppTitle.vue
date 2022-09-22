90a76f91928952b9d9a81ef818d32547e68f12dc
Denis-92 <d.mozenic@gmail.com>
2022-09-22 21:51:42 +0200

    aggiunto AppTitle con titolo

---
+++ dm-my-first-app-vue/src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="title">
+        <h1>La mia prima app con Vue CLI!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'firstApp',
+}
+</script>
+
+<style scoped lang="scss">
+h1 {
+    color: blueviolet;
+}
+</style>
